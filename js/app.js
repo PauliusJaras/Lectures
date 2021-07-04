73e4d868b4798626667b1b9602c90a6db0ba295c
Paulius Jaras <paulius.jaras@outlook.com>
2021-07-04 22:53:24 +0300

    New component LectureTable.js. and updated files

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 const studentList = localStorageService.students;
 const lectureList = localStorageService.lectures;
-
-console.log(studentList);
-
-
 const studentForm = new Form('#studentForm');
+const lectureForm = new Form('#lectureForm');
+
+console.log(lectureForm);
 
 studentForm.onSubmit(addStudent);
+lectureForm.onSubmit(addLecture);
 
 
 function addStudent(student){
     const student1 = new Student(1,student.name, student.course, []);
     studentList.push(student1);
     localStorageService.students = studentList;
+}
+
+function addLecture(lecture){
+    const lecture1 = new Lecture(1,lecture.title, lecture.course, 
+        lecture.studentLimit, lecture.startTime, lecture.endTime, []);
+    lectureList.push(lecture1);
+    localStorageService.lectures = lectureList;
 }
 
 function removeStudent(){
